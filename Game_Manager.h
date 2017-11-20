767e4b910b650f0b9020ae6d8554218f59650b4f
klatort <33676805+klatort@users.noreply.github.com>
2017-11-20 09:50:23 -0500

    Update Game_Manager.h

--- Game_Manager.h
+++ Game_Manager.h
@@ -5,14 +5,20 @@ class Game_Manager
 {
 	Tile ** T;
 	int *N_Tiles;
+	int	*N_Pollos;
+	Pollo** bolita;
 public:
-	Pollo* bolita;
+
 	Game_Manager();
 	~Game_Manager();
+	void Pollo_desaparece(System::Drawing::Graphics ^ g);
+	void Mover_pollos(System::Drawing::Graphics ^ g);
 	bool Colision(Figura* a, Figura* b);
 	void CheckColision();
 	void CheckImpact();
 	void Insertar_Tile(Tile* nuevo);
-	void Eliminar(int pos);
+	void Insertar_Pollo(Pollo * nuevo);
+	void Eliminar_Tile(int pos);
+	void Eliminar_Pollo(int pos);
 };
 
